58059d8ff3d37fb97ac181b48b128de11c1716a4
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-11-13 13:08:59 +0100

    build: update nix
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -9,30 +9,21 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-
-          /* config.allowUnfree = true; */
-          config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-            "ngrok"
-          ];
-        };
-        #pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages.${system};
         buildDeps = with pkgs; [ git go gnumake ];
-        devDeps = with pkgs; buildDeps ++ [ golangci-lint gotestsum ngrok ];
+        devDeps = with pkgs; buildDeps ++ [ golangci-lint gotestsum ];
 
-        generateGoEnv = go:
-          pkgs.buildEnv {
-            name = "go" + go.version;
-            paths = (pkgs.lib.remove pkgs.go devDeps) ++ [ go ];
+        goShell = go:
+          pkgs.mkShell {
+            buildInputs = (pkgs.lib.remove pkgs.go devDeps) ++ [ go ];
           };
       in {
         devShell = pkgs.mkShell {
           buildInputs = devDeps;
         };
 
-        packages.go1_15 = generateGoEnv pkgs.go_1_15;
-        packages.go1_16 = generateGoEnv pkgs.go_1_16;
-        packages.go1_17 = generateGoEnv pkgs.go_1_17;
+        devShells.go1_15 = goShell pkgs.go_1_15;
+        devShells.go1_16 = goShell pkgs.go_1_16;
+        devShells.go1_17 = goShell pkgs.go_1_17;
       });
 }
